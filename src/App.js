b55d5e89e9a8fd85c2137665af22ec1f9c86124f
elmehdi rhindi <g6handi@gmail.com>
2022-04-23 10:14:49 +0200

    file upload component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar } from "components";
+import { Navbar, FileUpload } from "components";
+import {useState} from 'react'
 
 function App() {
+  // variable to store file url
+  const [file, setFile] = useState()
+
   return (
     <div>
       <Navbar />
+      <FileUpload setFile={setFile}/>
+      <img src={file} />
       <Outlet />
       footer
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar";
 export { Signup } from "./Signup";
 export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
+export {FileUpload} from './FileUpload';
